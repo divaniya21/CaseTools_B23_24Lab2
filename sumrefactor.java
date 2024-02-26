d59bcc7dac5768eb7db23db7bb849617f0fb835f
divaniya21 <155934487+divaniya21@users.noreply.github.com>
2024-02-26 11:10:49 +0530

    Add files via upload

---
+++ sumrefactor.java
@@ -0,0 +1,9 @@
+public class sumrefactor {
+    public static void main(String args[])   
+{  
+int num1 = 225, num2 = 115, sum;  
+sum = num1 + num2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+    
+}
